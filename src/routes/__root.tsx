096c124de0aa0b6972ff01454ff050f30fb0f48c
erickjd61 <john@mudlabs.io>
2026-06-17 09:33:06 -0400

    Add explicit favicon + apple-touch link tags (Safari, bookmarks, iOS home screen)

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -86,6 +86,27 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
       ],
       links: [
         {
+          rel: 'icon',
+          href: '/favicon.ico',
+          sizes: 'any',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '32x32',
+          href: '/favicon-32x32.png',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '16x16',
+          href: '/favicon-16x16.png',
+        },
+        {
+          rel: 'apple-touch-icon',
+          href: '/apple-touch-icon.png',
+        },
+        {
           rel: 'stylesheet',
           href: appCss,
         },
